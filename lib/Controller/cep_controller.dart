7eb2d5c761eb9ac566e9f1f1cc65288b68262313
AnneAlmd <annealmd@gmail.com>
2022-01-21 20:05:12 -0300

    controller

--- lib/Controller/cep_controller.dart
+++ lib/Controller/cep_controller.dart
@@ -1,5 +1,15 @@
+import 'package:cep/Model/cep_model.dart';
+import 'package:cep/service/cep_service.dart';
 import 'package:flutter/cupertino.dart';
 
 class CepController extends ValueNotifier {
-  CepController(value) : super(value);
+  final CepService service;
+
+  late CepModel city;
+
+  CepController(this.service) : super(null);
+
+  Future fetchCep(String cep) async {
+    city = await service.fetchCep(cep);
+  }
 }
